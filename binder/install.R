4adc8897882fcc069f9ce1476f3bb707cff1b12c
Todd Iverson <Tiverson@winona.edu>
2019-04-17 13:01:10 -0500

    Added dplyr

--- binder/install.R
+++ binder/install.R
@@ -3,6 +3,7 @@
 install.packages("reticulate") # python support in RMarkdown
 install.packages("ggplot2") # for plotting
 install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
+install.packages("dplyr") # for plotting
 
 ### install bioconductor packages
 # install.packages("BiocManager")
